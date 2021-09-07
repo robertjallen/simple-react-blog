be141050c6f02c99aa8f3ca6c6a191d358913a61
Robert Allen <bobbidigi34@gmail.com>
2021-09-07 13:26:09 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 export default {
   bucket: {
-    slug: process.env.COSMIC_BUCKET || 'simple-react-blog',
+    slug: process.env.COSMIC_BUCKET || 'faf2f080-0d9b-11ec-8f7c-41a4f6c513b4',
     read_key: process.env.COSMIC_READ_KEY || ''
   }
 }
